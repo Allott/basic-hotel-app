d3ce81a83605d1b450e91d5aa3369f291098c5ea
Daniel Allott <36487931+Allott@users.noreply.github.com>
2023-11-20 19:08:59 +0000

    Basic TTD

--- src/test-intergration.test.js
+++ src/test-intergration.test.js
@@ -1,10 +1,41 @@
 import Index from './pages/index';
 import '@testing-library/jest-dom'
 import { screen, render } from '@testing-library/react';
+import hotelData from './constants/hotel-data';
+ 
 
 describe('Index', () => {
-    it('renders a heading', () => {
+    it('should render ', () => {
         render(<Index />);
-        expect(screen.getByText(/hello world/)).toBeInTheDocument();
+        expect(screen.getByText(hotelData[0].name)).toBeInTheDocument();
+        // ... more options
+    });
+    it('should sort by name', () => {
+        render(<Index />);
+        const alpha = screen.getByText(/alphabetically/);
+        expect(alpha).toBeInTheDocument();
+        // click
+        // check order changes
+    });
+    it('should sort by price', () => {
+        render(<Index />);
+        const price = screen.getByText(/price/);
+        expect(price).toBeInTheDocument();
+        // click
+        // check order changes
+    });
+    it('should sort by star rating', () => {
+        render(<Index />);
+        const star = screen.getByText(/star rating/);
+        expect(star).toBeInTheDocument();
+        // click
+        // check order changes
+    });
+    it('should show description on click', () => {
+        render(<Index />);
+        const description = screen.getByAllText(/Read more/);
+        expect(description.length).toBe(3);
+        // click
+        // check description is shown
     });
 });
